54a9e925dd45d19aadc4911c94d4dab244fcb34c
Landon Skinner <landonjskinner@gmail.com>
2021-11-02 16:23:39 -0500

    commit tree 9244dd87b763750a264840f1cb385d0fe4646175

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
+
+
+
+
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+  const updateInCart = () => {
+    setInCart(!inCart)
+  }
   return (
-    <li className="">
+    <li className={inCart === false ? "" : "in-cart"}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={updateInCart}>{inCart === false ? 'Add to Cart' : 'Remove from Cart'}</button>
     </li>
   );
 }
